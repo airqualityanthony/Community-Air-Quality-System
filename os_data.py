a1ac8b8d59946941b4d37114931686240c438dbc
Anthony Walker <45573448+airqualityanthony@users.noreply.github.com>
2023-08-10 16:52:41 +0100

    Initial commit - OS data API & plotting templates

--- os_data.py
+++ os_data.py
@@ -1,7 +1,7 @@
 
 from osdatahub import FeaturesAPI, Extent
 
-key = "tiZBA5AnyMgDvPt5nbADwzhs8oGpy0pJ"
+key = os.environ.get('OS_API_KEY')
 bbox = (-2.2361, 53.4746, -2.2234, 53.4805)
 extent = Extent.from_bbox(bbox, "EPSG:4326")
 
