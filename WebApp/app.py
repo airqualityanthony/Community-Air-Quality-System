19a2d2652231e5c4058a0b4d07f2a9442e06d660
Anthony Walker <45573448+airqualityanthony@users.noreply.github.com>
2023-08-29 15:46:35 +0100

    WebApp Tempalte upload

---
+++ WebApp/app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import geemap.foliumap as geemap
+
+st.set_page_config(layout="wide")
+
+# Customize the sidebar
+markdown = """
+Web App URL: <https://geemap.streamlit.app>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+# Customize page title
+st.title("Earth Engine Web App")
+
+st.markdown(
+    """
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [geemap](https://geemap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/geemap-apps).
+    """
+)
+
+st.header("Instructions")
+
+markdown = """
+1. For the [GitHub repository](https://github.com/giswqs/geemap-apps) or [use it as a template](https://github.com/new?template_name=geemap-apps&template_owner=giswqs) for your own project.
+2. Customize the sidebar by changing the sidebar text and logo in each Python files.
+3. Find your favorite emoji from https://emojipedia.org.
+4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+"""
+
+st.markdown(markdown)
+
+st.title("Marker Cluster")
+
+with st.expander("See source code"):
+    with st.echo():
+
+        m = geemap.Map(center=[40, -100], zoom=4)
+        cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
+        regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
+
+        m.add_geojson(regions, layer_name='US Regions')
+        m.add_points_from_xy(
+            cities,
+            x="longitude",
+            y="latitude",
+            color_column='region',
+            icon_names=['gear', 'map', 'leaf', 'globe'],
+            spin=True,
+            add_legend=True,
+        )
+
+m.to_streamlit(height=700)
