6c9e5daf377a84f372e5880ea1e365f2842f5fd6
Anthony Walker <45573448+airqualityanthony@users.noreply.github.com>
2023-08-29 22:29:49 +0100

    page folder temp rename

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-Web App URL: <https://geemap.streamlit.app>
-"""
+Citizen Air Quality System
+CAQS is a citizen science project that aims to provide an open-source, 
+and easy-to-use air quality modelling system for the public."""
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
@@ -25,32 +26,19 @@ st.markdown(
 st.header("Instructions")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/giswqs/geemap-apps) or [use it as a template](https://github.com/new?template_name=geemap-apps&template_owner=giswqs) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+1. Place a marker using the toolset on the left of the map 
+2. Select which year you would like to predict the concentration at the point for. 
+3. Hit Submit and wait for the model to run.
+4. Once complete head to the Modelling Output page to see the results.
 """
 
 st.markdown(markdown)
 
-st.title("Marker Cluster")
+st.title("Map")
 
 with st.expander("See source code"):
     with st.echo():
 
-        m = geemap.Map(center=[40, -100], zoom=4)
-        cities = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
-        regions = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson'
-
-        m.add_geojson(regions, layer_name='US Regions')
-        m.add_points_from_xy(
-            cities,
-            x="longitude",
-            y="latitude",
-            color_column='region',
-            icon_names=['gear', 'map', 'leaf', 'globe'],
-            spin=True,
-            add_legend=True,
-        )
+        m = geemap.Map(center=[51, -1], zoom=4)
 
 m.to_streamlit(height=700)
