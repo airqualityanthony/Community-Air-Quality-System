c3543ac130637a16890247ca6618c3a1918daf31
Anthony Walker <45573448+airqualityanthony@users.noreply.github.com>
2023-09-11 11:16:18 +0000

    adding earth engine CAMS data, and updating output

--- app.py
+++ app.py
@@ -13,17 +13,18 @@ and easy-to-use air quality modelling system for the public."""
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
+logo = "https://i.dailymail.co.uk/i/pix/2016/12/01/17/3AE49E8700000578-3986672-AirVisual_Earth_aims_to_clearly_show_the_effect_that_human_emiss-m-25_1480613072849.jpg"
 st.sidebar.image(logo)
 
 # Customize page title
-st.title("Earth Engine Web App")
+st.title("Citizen Air Quality System - Web App")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [geemap](https://geemap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/giswqs/geemap-apps).
+    This web app allows users to place markers on the below interactive map, and leverage open source APIs to automate and conduct air quality concentration modelling
     """
 )
+
 
 st.header("Instructions")
 
@@ -38,7 +39,7 @@ st.markdown(markdown)
 
 st.title("Map")
 
-m = geemap.Map(center=[51, -1], 
+m = geemap.Map(center=[54, 1], 
                 zoom=6,
                 draw_export=True)
 
@@ -47,9 +48,25 @@ m = geemap.Map(center=[51, -1],
 mapdata = st_folium(m, width=1500, height=700)
 
 ## Write Drawing Data to Streamlit
-st.write(mapdata['all_drawings'])
+longitudes = []
+latitudes = []
+
+# st.write(len(mapdata['all_drawings']))
+
+if mapdata['all_drawings']:
+    for i in range(0,len(mapdata['all_drawings'])):
+        longitudes.append(mapdata['all_drawings'][i]['geometry']['coordinates'][0])
+        latitudes.append(mapdata['all_drawings'][i]['geometry']['coordinates'][1])
+    st.write(longitudes)
+    st.write(latitudes)
+
+else: 
+    st.write("Please select markers before continuing.")
+
+
+
+st.selectbox('Select Year', range(2023,2040))
 
 if st.button("Submit Model Coordinates"):
-    # roi = ee.FeatureCollection(m.draw_features)
-    # st.write(roi.getInfo())
+
     pass
---
+++ pages/EarthEngineCAMS.py
@@ -0,0 +1,45 @@
+import ee
+import streamlit as st
+import geemap.foliumap as geemap
+from streamlit_folium import st_folium
+import datetime
+
+
+st.header("CAMS NO2 monitoring")
+# Create an interactive map
+Map = geemap.Map(center=[40, -100], zoom=4)
+
+# Get an NLCD image by year.
+
+dataset = ee.ImageCollection('ECMWF/CAMS/NRT')
+
+# Use an ee.DateRange object.
+start_date = st.date_input("Start Date", datetime.date(2022, 7, 1))
+end_date = st.date_input("End Date", datetime.date(2022, 8, 1))
+
+date_range = ee.DateRange(start_date, end_date)
+data = dataset.filterDate(date_range)
+
+
+##Select first and last forecast hours.
+hour00 = data.filter('model_forecast_hour == 0').first()
+hour21 = data.filter('model_forecast_hour == 21').first()
+
+
+
+## Visualization parameters for specified aerosol band.
+visParams = {
+  'bands': ['total_aerosol_optical_depth_at_550nm_surface'],
+  'min': 0.0,
+  'max': 3.6,
+  'palette': ['5e4fa2', '3288bd', '66c2a5', 'abe0a4', 'e6f598', 'ffffbf',
+    'fee08b', 'fdae61', 'f46d43', 'd53e4f', '9e0142']
+}
+
+# Display forecasts on the map.
+Map.setCenter(70, 45, 3)
+Map.addLayer(hour00, visParams, 'total_column_nitrogen_dioxide_surface - H00', 'true', 0.6)
+Map.addLayer(hour21, visParams, 'total_column_nitrogen_dioxide_surface - H21', 'true', 0.6)
+
+st_folium(Map, width=1500, height=700)
+
